cca7329138581154cea00662661cdf1110e0bd27
Arthur Silva <arthursns@gmail.com>
2021-10-24 12:17:02 -0300

    Resolução 1041

---
+++ 1041 - Coordenadas de um Ponto/1041.py
@@ -0,0 +1,20 @@
+inputS = input().split()
+
+x, y = inputS
+x = float(x)
+y = float(y)
+
+if (x > 0 and y > 0):
+    print("Q1")
+elif (x < 0 and y > 0):
+    print("Q2")
+elif (x < 0 and y < 0):
+    print("Q3")
+elif (x > 0 and y < 0):
+    print("Q4")
+elif (x == 0 and y == 0):
+    print("Origem")
+elif (y == 0):
+    print("Eixo X")
+elif (x == 0):
+    print("Eixo Y")
