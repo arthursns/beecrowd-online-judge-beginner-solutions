96bd9d75ecd58d9690f4836d70cfbfb6ee52723a
Arthur Silva <arthursns@gmail.com>
2021-11-07 22:01:52 -0300

    Resolução 1065

---
+++ 1065 - Pares entre Cinco Números/1065.py
@@ -0,0 +1,23 @@
+a = int(input())
+contagem = 0
+
+if a%2 == 0:
+    contagem = contagem + 1
+
+b = int(input())
+if b%2 == 0:
+    contagem = contagem + 1
+
+c = int(input())
+if c%2 == 0:
+    contagem = contagem + 1
+
+d = int(input())
+if d%2 == 0:
+    contagem = contagem + 1
+
+e = int(input())
+if e%2 == 0:
+    contagem = contagem + 1
+
+print(str(contagem) + " valores pares")
