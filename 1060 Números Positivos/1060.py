76ffbf9b362fa6e541d211c691f020fcbfcb55d8
Arthur Silva <arthursns@gmail.com>
2021-11-07 21:52:25 -0300

    Resolução 1060

---
+++ 1060 Números Positivos/1060.py
@@ -0,0 +1,27 @@
+a = float(input())
+contagem = 0
+
+if a > 0:
+    contagem = contagem + 1
+
+b = float(input())
+if b > 0:
+    contagem = contagem + 1
+
+c = float(input())
+if c > 0:
+    contagem = contagem + 1
+
+d = float(input())
+if d > 0:
+    contagem = contagem + 1
+
+e = float(input())
+if e > 0:
+    contagem = contagem + 1
+
+f = float(input())
+if f > 0:
+    contagem = contagem + 1
+
+print(str(contagem) + " valores positivos")
